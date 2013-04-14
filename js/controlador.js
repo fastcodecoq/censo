92ab9760976f7ba5d7275ebfa0f7c92254215877
Gomosöft <info@gomosoft.com>
2013-04-14 17:47:46 -0500

    agregado evento para detectar conexion

--- js/controlador.js
+++ js/controlador.js
@@ -85,6 +85,13 @@ var socket = io.connect('http://apmontelibano.com:8888');
 
                     break;
 
+
+                    case "cargar":
+
+                      checkConnection();
+
+                    break;
+
                 }
 
 
@@ -151,6 +158,34 @@ var socket = io.connect('http://apmontelibano.com:8888');
 
 
       //===================================
+
+
+
+      // eventos de conexion
+
+
+      function checkConnection() {
+
+         var networkState = navigator.network.connection.type;
+
+         var states = {};
+
+            states[Connection.UNKNOWN]  = 'Conexión desconocida';
+            states[Connection.ETHERNET] = 'Conexión ethernet';
+            states[Connection.WIFI]     = 'Conexión WiFi';
+            states[Connection.CELL_2G]  = 'Conexión movil 2G';
+            states[Connection.CELL_3G]  = 'Conexión movil 3G';
+            states[Connection.CELL_4G]  = 'Conexión movil 4G';
+            states[Connection.NONE]     = 'Sin conexión';
+
+        alert('Tipo de conexión: ' + states[networkState]);
+
+    }
+
+
+
+
+      //================================
 
 
 
